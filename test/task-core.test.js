c0af35fa56ee2bfb6a3e8a6c927641b0be6754e3
Panagiotis Papadatis <hennett.afa@gmail.com>
2018-07-27 17:32:21 +0300

    [WIP] update tests

--- test/task-core.test.js
+++ test/task-core.test.js
@@ -1,14 +1,7 @@
 const debug = require('debug')('ck');
 const BigNumber = require('bignumber.js');
-
-const ETH_STRING = web3.toWei(1, 'ether');
-const FINNEY_STRING = web3.toWei(1, 'finney');
-const ETH_BN = new BigNumber(ETH_STRING);
-const FINNEY_BN = new BigNumber(FINNEY_STRING);
-const NULL_ADDRESS = '0x0000000000000000000000000000000000000000';
-
 const util = require('./util.js');
-const task = artifacts.require('./TaskCore');
+const taskCore = artifacts.require('./TaskCore');
 
 contract('TaskCore', (accounts) => {
   before(() => util.measureGas(accounts));
@@ -18,37 +11,138 @@ contract('TaskCore', (accounts) => {
 
   const tomorrow = Date.now() + 24 * 60 * 60;
   const yesterday = Date.now() - 24 * 60 * 60;
+  const nullAddress = '0x0000000000000000000000000000000000000000';
   const serviceOwner = accounts[0];
   const user = accounts[1];
   const supervisor1 = accounts[2];
   const supervisor2 = accounts[3];
-  const gasPrice = 1e11;
-  let coreC;
+  const newServiceOwner = accounts[4];
   const logEvents = [];
   const pastEvents = [];
+  let coreC;
 
   after(function() {
     logEvents.forEach((ev) => ev.stopWatching());
   });
 
-  it('deploys the contract', async () => {
-    await task.deployed();
-    assert(task.address);
+  async function deployContract() {
+    await taskCore.deployed();
+    coreC = await taskCore.new();
+
+    const eventsWatch = coreC.allEvents();
+    eventsWatch.watch((err, res) => {
+      if (err) return;
+      pastEvents.push(res);
+      debug('>>', res.event, res.args);
+    });
+    logEvents.push(eventsWatch);
+  }
+
+  describe('initial state', () => {
+    before(deployContract);
+
+    it('deploys the contract', async () => {
+      assert(taskCore.address);
+    });
+
+    it('assigns the creator as a service owner', async () => {
+      const owner = await coreC.serviceOwner();
+      assert.equal(owner, serviceOwner);
+    });
   });
 
-  // it('marks caller as the procrastinator', async () => {
-  //   await factory.deployed();
-  //   const task = await factory
-  //     .createTask('complete a task', tomorrow)
-  //     .send({
-  //       from: accounts[0],
-  //       value: web3.utils.toWei('3', 'finney'),
-  //       gas: '1000000',
-  //     });
+  describe('access control functionality', () => {
+    before(deployContract);
 
-  //   const procrastinator = await task.procrastinator().call();
-  //   assert.equal(accounts[0], procrastinator);
-  // });
+    it('transfers ownership to another address', async () => {
+      await coreC.transferOwnership(newServiceOwner);
+      const owner = await coreC.serviceOwner();
+      assert.equal(owner, newServiceOwner);
+    });
+  });
+
+  describe('core functionality', () => {
+    before(deployContract);
+
+    it('creates a new task', async () => {
+      await coreC.createTask('goal', tomorrow, {
+        from: user,
+        value: web3.toWei('2', 'finney'),
+      });
+
+      assert(coreC.ownershipTaskCount[user] > 0);
+    });
+
+    it('returns task details by id', async () => {
+      const taskC = await coreC.createTask('goal', tomorrow, {
+        from: user,
+        value: web3.toWei('2', 'finney'),
+      });
+
+      const details = await taskC.getTask(0).call();
+      assert(details);
+    });
+
+    it('denies a task if the user is null', async () => {
+      try {
+        await coreC.createTask('goal', tomorrow, {
+          from: nullAddress,
+          value: web3.toWei('2', 'finney'),
+        });
+
+        assert(false);
+      } catch (err) {
+        assert(err);
+      }
+    });
+
+    it('denies a task if the user payes less than the minimum', async () => {
+      try {
+        await coreC.createTask('goal', tomorrow, {
+          from: user,
+          value: web3.toWei('1', 'finney'),
+        });
+
+        assert(false);
+      } catch (err) {
+        assert(err);
+      }
+    });
+
+    it('denies a task if the user does not speficy a goal', async () => {
+      try {
+        await coreC.createTask(null, tomorrow, {
+          from: user,
+          value: web3.toWei('2', 'finney'),
+        });
+
+        assert(false);
+      } catch (err) {
+        assert(err);
+      }
+    });
+
+    it('denies a task if the user specifies a date earlier than today', async () => {
+      try {
+        await coreC.createTask('goal', yesterday, {
+          from: user,
+          value: web3.toWei('2', 'finney'),
+        });
+
+        assert(false);
+      } catch (err) {
+        assert(err);
+      }
+    });
+  });
+
+  describe('base functionality', () => {
+    before(deployContract);
+  });
+
+  describe('ownership functionality', () => {
+    before(deployContract);
+  });
 
   // it('allows a procrastinator to set a supervisor', async () => {
   //   await factory.deployed();
