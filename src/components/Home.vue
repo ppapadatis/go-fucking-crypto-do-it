7bb66405901b8aa1fd2f5f15b8fcf0215d80f295
Panagiotis Papadatis <hennett.afa@gmail.com>
2018-08-03 02:10:17 +0300

    [WIP] minor validation updates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,9 +3,14 @@
         <el-col :span="16" :offset="4">
             <el-form ref="task" :model="task" v-loading="loading" :element-loading-text="loadingText" :rules="rules" @validate="onValidate">
                 <el-form-item>
-                  <app-steps :active="active"></app-steps>
+                  <el-steps :active="active" finish-status="success">
+                    <el-step title="Step 1" description="Set Goal"></el-step>
+                    <el-step title="Step 2" description="Set Deadline"></el-step>
+                    <el-step title="Step 3" description="Set Supervisor"></el-step>
+                    <el-step title="Step 4" description="Summary"></el-step>
+                </el-steps>
                 </el-form-item>
-                <el-form-item label="Goal Description" v-show="active === 0" prop="goal">
+                <el-form-item label="What's your goal?" v-show="active === 0" prop="goal">
                     <el-input
                         autofocus
                         minlength="1"
@@ -13,23 +18,25 @@
                         type="textarea"
                         resize="none"
                         :rows="2"
-                        placeholder="Enter your goal's description here..."
+                        placeholder="Enter your goal"
                         v-model="task.goal">
                     </el-input>
-                    <span class="text-smaller">{{charactersLeft}} characters left.</span>
                 </el-form-item>
-                <el-form-item label="Pick Deadline" v-show="active === 1" prop="deadline">
+                <el-form-item v-show="active === 0">
+                  <span class="text-smaller">{{charactersLeft}} characters left.</span>
+                </el-form-item>
+                <el-form-item label="Set Deadline" v-show="active === 1" prop="deadline">
                     <el-date-picker
                         :picker-options="datePickerOptions"
                         v-model="task.deadline"
                         type="date"
-                        placeholder="Pick a day"
+                        placeholder="Pick a date"
                         :editable="false">
                     </el-date-picker>
                 </el-form-item>
-                <el-form-item label="Supervisor Address" v-show="active === 2" prop="supervisor">
+                <el-form-item label="Set a supervisor" v-show="active === 2" prop="supervisor">
                   <el-input
-                    placeholder="Enter a supervisor (eth) address"
+                    placeholder="Enter an ethereum address"
                     v-model="task.supervisor">
                   </el-input>
                 </el-form-item>
@@ -43,17 +50,17 @@
                     <p>Supervisor: <a :href="'https://etherscan.io/address/' + task.supervisor" target="_blank" rel="noopener noreferrer">{{ task.supervisor }}</a></p>
                     <hr/>
                     <p>
-                      Please enter the value to stake:
+                      Set Price
                       <el-input-number v-model="task.stake" :min="minimumStake"></el-input-number>
                     </p>
                 </el-card>
                 </el-form-item>
                 <el-form-item>
                     <el-button native-type="button" type="primary" @click="prev" :disabled="active < 1 || loading" plain round>Previous step</el-button>
-                    <el-button native-type="submit" type="primary" @click.prevent="next('task')" name="submit" :disabled="disabledButton" :loading="loading" round>
-                      Next step
+                    <el-button :icon="buttonState[active].icon" native-type="submit" type="primary" @click.prevent="next('task')" name="submit" :disabled="disabledButton" :loading="loading" round>
+                      {{ buttonState[active].title }}
                     </el-button>
-                    <el-button native-type="button" type="warning" @click="reset('task')" :disabled="task.goal.length < 1 || loading" plain round>Reset</el-button>
+                    <el-button native-type="button" type="warning" @click="reset('task')" :disabled="!task.goal || loading" plain round>Reset</el-button>
                 </el-form-item>
             </el-form>
         </el-col>
@@ -62,7 +69,6 @@
 
 <script>
 import mixin from '../utils/mixinViews'
-import Steps from './partials/Steps.vue'
 
 export default {
   name: 'Home',
@@ -77,6 +83,10 @@ export default {
         value.toLowerCase() === '0x0000000000000000000000000000000000000000'
       ) {
         callback(new Error('Not a valid Ethereum address'))
+      } else if (!window.web3.eth.coinbase) {
+        callback(
+          new Error('Unlock your metamask account to enable further validation')
+        )
       } else if (
         value.toLowerCase() === window.web3.eth.coinbase.toLowerCase()
       ) {
@@ -93,6 +103,13 @@ export default {
       loading: false,
       loadingText:
         'Please wait until the process is finished successully.\nDo not close or reload this tab!',
+      buttonState: [
+        { icon: 'el-icon-edit', title: 'Set Goal' },
+        { icon: 'el-icon-date', title: 'Set Deadline' },
+        { icon: 'el-icon-view', title: 'Set Supervisor' },
+        { icon: 'el-icon-check', title: 'Go Fucking - Crypto - Do It' },
+        { icon: 'el-icon-loading', title: 'Creating Task...' }
+      ],
       datePickerOptions: {
         disabledDate(date) {
           return date <= new Date()
@@ -105,18 +122,18 @@ export default {
       },
       task: {
         goal: '',
-        deadline: '',
+        deadline: Date.now(),
         supervisor: '',
         stake: 0
       },
       rules: {
         goal: [
-          { required: true, message: 'Please input Goal', trigger: 'blur' },
+          { required: true, message: 'Please input Goal', trigger: 'change' },
           {
             min: 1,
             max: 140,
             message: 'Length should be 1 to 140',
-            trigger: 'blur'
+            trigger: 'change'
           }
         ],
         deadline: [
@@ -131,9 +148,9 @@ export default {
           {
             required: true,
             message: 'Supervisor address can not be null',
-            trigger: 'blur'
+            trigger: 'change'
           },
-          { validator: checkEthereumAddress, trigger: 'blur' }
+          { validator: checkEthereumAddress, trigger: 'change' }
         ]
       }
     }
@@ -152,7 +169,14 @@ export default {
         return true
       }
 
-      if (this.active > 1 && !this.valid['supervisor']) {
+      if (this.active === 2 && !this.valid['supervisor']) {
+        return true
+      }
+
+      if (
+        this.active === 3 &&
+        (!this.task.stake || this.task.stake < this.minimumStake)
+      ) {
         return true
       }
 
@@ -172,49 +196,31 @@ export default {
       this.valid[prop] = valid
     },
     prev() {
-      if (this.active > 2) {
-        Event.$emit('lastStepUpdated', 'wait')
-        this.active = 2
-      } else if (this.active > 0) {
+      if (this.active > 0) {
         this.active--
       }
-
-      document.getElementsByName('submit')[0].innerHTML = 'Next step'
     },
     next() {
-      if (this.active >= 4) {
-        Event.$emit('lastStepUpdated', 'success')
-        this.loading = true
-        this.submitForm('task')
-        return false
-      }
-
-      if (this.active < 3) {
+      if (this.active < 4) {
         this.active++
       }
 
-      if (this.active > 2 && !this.disabledButton) {
-        this.active = 4
-        Event.$emit('lastStepUpdated', 'process')
-        event.target.innerHTML = 'Create Goal'
+      if (this.active === 4) {
+        this.loading = true
+        this.submitForm('task')
       }
     },
     reset(formName) {
       this.active = 0
       this.loading = false
-      Event.$emit('lastStepUpdated', 'wait')
       this.valid = {
         goal: false,
         deadline: false,
         supervisor: false
       }
-      this.task = {
-        goal: '',
-        deadline: '',
-        supervisor: ''
-      }
-      document.getElementsByName('submit')[0].innerHTML = 'Next step'
       this.$refs[formName].resetFields()
+      this.task.stake = this.minimumStake
+      this.task.deadline = Date.now()
     },
     submitForm(formName) {
       this.$refs[formName].validate(valid => {
@@ -232,6 +238,7 @@ export default {
                 if (err) {
                   this.$message.error(err.message)
                   this.loading = false
+                  this.active = 3
                   return false
                 } else {
                   Event.$emit('taskCreated', txHash)
@@ -242,11 +249,13 @@ export default {
           } catch (error) {
             this.$message.error(error.message)
             this.loading = false
+            this.active = 3
             return false
           }
         } else {
           this.$message.error('Form could not be submitted due to errors')
           this.loading = false
+          this.active = 3
           return false
         }
       })
@@ -271,10 +280,6 @@ export default {
           }
         })
     }
-  },
-
-  components: {
-    appSteps: Steps
   }
 }
 </script>
